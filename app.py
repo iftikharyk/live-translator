3f04f779ee920e4955da129c97087f397c851b03
iftikhar <iftikhar@sixlogs.com>
2023-06-16 16:49:10 +0500

    functions

--- app.py
+++ app.py
@@ -1,31 +1,57 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import JSONFormatter
-import json
+from youtube_transcript_api.formatters import TextFormatter
+from youtube_transcript_api.formatters import WebVTTFormatter
+from youtube_transcript_api.formatters import SRTFormatter
 import csv
 import pandas as pd
-formatter = JSONFormatter()
-
-transcript = YouTubeTranscriptApi.get_transcript('z3fmTwXv1WQ')
-
-# .format_transcript(transcript) turns the transcript into a JSON string.
-json_formatted = formatter.format_transcript(transcript)
-
-# Now we can write it out to a file.
-with open('your_filename.json', 'w', encoding='utf-8') as json_file:
-    json_file.write(json_formatted)
-
-# try:
-#     transcript = YouTubeTranscriptApi.get_transcript('MJEyGsn1C40')
-#     # z3fmTwXv1WQ MJEyGsn1C40
-# except:
+jsonFormatter = JSONFormatter()
+srtFormatter = SRTFormatter()
     
 
+def saveAsJSON(dict):
+    # .format_transcript(transcript) turns the transcript into a JSON string.
+    json_formatted = jsonFormatter.format_transcript(dict)
 
-print(transcript)
+    # Now we can write it out to a file.
+    with open('your_filename.json', 'w', encoding='utf-8') as json_file:
+        json_file.write(json_formatted)
 
-# header = ['text', 'start', 'duration']
+def saveAsSRT(dict):
+    srt_formatted = srtFormatter.format_transcript(dict)
 
-# with open('transcript.csv', 'w', encoding='UTF8', newline='') as f:
-#     writer = csv.DictWriter(f, fieldnames=header)
-#     writer.writeheader()
-#     writer.writerows(transcript)
+    with open('subtitles.srt', 'w', encoding='utf-8') as srt_file:
+        srt_file.write(srt_formatted)
+
+def saveAsCSV(dict):
+    header = ['text', 'start', 'duration']
+
+    with open('transcript.csv', 'w', encoding='UTF8', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=header)
+        writer.writeheader()
+        writer.writerows(dict)
+
+def saveAsTXT(dict):
+    with open('example.txt', 'w') as file:
+        for obj in dict:
+            file.write(obj['text'] + '\n')
+
+def loadTranscript(videoId):
+    try:
+        transcript = YouTubeTranscriptApi.get_transcript(videoId)
+
+        saveAsJSON(transcript)
+        saveAsCSV(transcript)
+        saveAsTXT(transcript)
+        saveAsSRT(transcript)
+        print('Done')
+    
+    except:
+        print('Error')
+    
+
+#contains transcript
+loadTranscript('z3fmTwXv1WQ')
+
+#does not contains transcript
+# loadTranscript('MJEyGsn1C40')
